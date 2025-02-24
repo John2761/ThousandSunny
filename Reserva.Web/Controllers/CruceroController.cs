ebd1070287a314b97a19d22b71e7a33bb9cfbcf1
Rusty10P <perez.kisha04@gmail.com>
2025-02-23 21:17:02 -0600

    Controller Crucero

---
+++ Reserva.Web/Controllers/CruceroController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reserva.Application.Services.Interfaces;
+
+namespace Reserva.Web.Controllers
+{
+    public class CruceroController : Controller
+    {
+        private readonly IServiceCrucero _serviceCrucero;
+
+        public CruceroController(IServiceCrucero serviceCrucero)
+        {
+            _serviceCrucero = serviceCrucero;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var crucero = await _serviceCrucero.ListAsync();
+            return View(crucero);
+        }
+
+        public async Task<IActionResult> Detalle(int id)
+        {
+            var crucero = await _serviceCrucero.FindByIdAsync(id);
+            if (crucero == null) 
+            {
+                return NotFound();
+            }
+            return View(crucero);
+        }
+    }
+}
